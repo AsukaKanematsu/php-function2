8bd211532651052592ce61f049990718ec45a727
AsukaKanwmatsu <asuka.kanematsu@en-gei.co.jp>
2023-05-04 12:30:04 +0900

    [PHP基礎]関数の課題2の提出

--- src/public/index.php
+++ src/public/index.php
@@ -24,7 +24,46 @@ $tweets = [
         'tweets' => '寿司を食べたい',
         'created_at' => '2022/10/25',
     ],
-]; ?>
+]; 
+
+$tweets= createTweetSatou($tweets);
+
+// $tweetSatou = [];
+
+// foreach($tweets as $tweet) {
+//     // if ($tweet["userName"] == "さとう") {
+//     //     $tweetSatou [] = $tweet;
+//     // }
+//     if (isSatou($tweet)) {
+//         $tweetSatou [] = $tweet;
+        
+//     }
+    
+// }
+
+function isSatou(array $tweet): bool 
+{
+    return $tweet["userName"] == "さとう";
+}
+
+function createTweetSatou(array $tweets): array 
+{
+    $tweetSatou = [];
+    foreach($tweets as $tweet) {
+        // if ($tweet["userName"] == "さとう") {
+        //     $tweetSatou [] = $tweet;
+        // }
+        if (isSatou($tweet)) {
+            $tweetSatou [] = $tweet;
+            
+        }
+        
+    }
+    return $tweetSatou;
+}
+?>
+
+
 
 <!-- 以下はHTMLのコードになります -->
 <!-- 今は「こんな処理をしているんだな〜」とざっくり見ていただけたらと思います！ -->
